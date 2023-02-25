0152e6ca34467ba9d7aa4c0f76ac415f678cfa5d
Div-Goel <118044120+Div-Goel@users.noreply.github.com>
2023-02-25 15:56:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let mybutton = document.getElementById("button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+  {
+    mybutton.style.display = "block";
+  } 
+  else 
+  {
+    mybutton.style.display = "none";
+  }
+}
